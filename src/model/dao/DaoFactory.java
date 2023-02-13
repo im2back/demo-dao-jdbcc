1254dcbd65cc66716bf894e82ddec8dcc7dabd3f
Jefferson Richards <jefferson.richards@outlook.com>
2023-02-13 18:36:20 -0300

    DepartmentDao implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartamentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 // esta classe tem operações estaticas para isntanciar o DAO
@@ -9,4 +10,8 @@ public class DaoFactory {
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
 	}
+		public static DepartamentDao createDepartamentDao() {
+			return new DepartamentDaoJDBC(DB.getConnection());
+		}
+	
 }
